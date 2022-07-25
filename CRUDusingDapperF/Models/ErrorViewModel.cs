c4d70498b55651c6c6110804d361c51dfb8f0185
Abhijeet <Abhijeet@DESKTOP-LQC2102>
2022-07-25 12:34:58 +0530

    Add project files.

---
+++ CRUDusingDapperF/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CRUDusingDapperF.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
